404a496c37ff5041cb103afb60c562c8545967f0
Anton-Gabriel-code <antoniothecode@gmail.com>
2024-11-04 15:47:32 -0300

    intro_python2.py

--- intro_python2.py
+++ intro_python2.py
@@ -3,14 +3,6 @@ lista_1 = [10, 20, 30]
 lista_1.extend([40, 50, 60]) #Adiciona um valor no final da lista atual
 print(lista_1)
 
-frase = ("Um elefante incomoda muita gente")
-frase[3:20]#fatia a string do começo da string até o final sempre conta - se de 0 a 4
-
-x = int(input("Digite um numero: ")) #int significa que somente números inteiros vão ser adicionados
-y = int(input("Digite outro numero: "))
-z = (x**2 + y**2)/(x-y)**2
-print(z)
-
 lista_1 = [10, 20, 30]
 del lista_1[1] # "del" remove um item da lista de acordo com o indice, sempre contando de 0 a 4
 print(lista_1)
@@ -41,7 +33,7 @@ print("PRODUTOS","PREÇO")
 cardapio = {"Salgado": 4.50, "Lanche": 6.50, "Suco": 3.00, "Refrigerante": 3.50, "Doce": 1.00} #cardapio é um dicionario, que é definido por um conjunto de valores, onde cada valor é associado a uma chave de acesso.
 print(cardapio)
 
-N = {"Antônio": 100, "Joyce": 90, "Arielly": 0, "Duda Moreira": 80, "Duda Villeroy": 80}
+N = {"Antônio": 100, "Simon": 90, "Alvin": 0, "Violet": 80, "Theodore": 80}
 M = sum(N.values())/len(N)
 print("Média da turma:", M)
 
