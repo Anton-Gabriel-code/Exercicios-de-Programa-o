8e13de9a8e8d4c43802ee16bfc7badc3b6d71a5e
Anton-Gabriel-code <antoniothecode@gmail.com>
2024-11-04 16:41:53 -0300

    exercicios_est-decisao.py

--- exercicios_est-decisao.py
+++ exercicios_est-decisao.py
@@ -5,3 +5,16 @@ elif pontos >= 1000 and pontos <= 5000:
   print("BOA! Você é intermediário!")
 elif pontos > 5000:
   print("PARABÉNS!!! Você é avançado!")
+
+
+m = float(input("Insira sua primeira nota: "))
+m1 = float(input("Insira sua segunda nota: "))
+media = (m + m1)/2
+print(media)
+if media >= 7:
+  print("Parabéns!!! Você está aprovado!")
+elif media >= 5 and  media <= 6.9:
+  print("Que pena, você está de recuperação!")
+elif media < 5:
+  print("Que pena, você está reprovado!")
+
