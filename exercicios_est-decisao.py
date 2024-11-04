bfcae44e78921e04b0ae24afe5df1fefb11d51d5
Anton-Gabriel-code <antoniothecode@gmail.com>
2024-11-04 17:12:01 -0300

    exercicios_est-decisao.py

--- exercicios_est-decisao.py
+++ exercicios_est-decisao.py
@@ -29,3 +29,12 @@ elif renda >= 50000:
     imposto = renda * a_a
 print(f"O valor do imposto de renda a ser pago é de R$ {imposto:.2f}")
 
+p = float(input("Insira o preço do seu produto: "))
+p1 = float(input("Insira o preço do seu produto: "))
+p2 = float(input("Insira o preço do seu produto: "))
+if p and p1 and p2 <100:
+  print("Este produtos são baratos!")
+elif p and p1 and p2 >=100 or p and p1 and p2 <=500:
+  print("Este produtos estão com o preço moderado!")
+elif p and p1 and p2 > 500:
+  print("Este produtos são caros!")
